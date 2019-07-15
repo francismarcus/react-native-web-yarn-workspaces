64d8c757c67917560ba5384c4e5842232bde42a9
Marcus Francis <marcusmagnusson@outlook.com>
2019-07-15 16:27:05 +0200

    react native web init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { View, Text, StyleSheet } from 'react-native'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View style={styles.box}>
+      <Text style={styles.heading}> React </Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  box: { 
+    alignItems: 'center',
+  },
+  heading: { 
+    fontWeight: 'bold',
+    fontSize: 48
+  }
+});
+
 
 export default App;
